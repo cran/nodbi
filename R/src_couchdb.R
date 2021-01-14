89e06a330b6689142a7e74d094b9728ea2004669
Scott Chamberlain <sckott@protonmail.com>
2021-01-14 21:50:06 +0000

    version 0.4.2

--- R/src_couchdb.R
+++ R/src_couchdb.R
@@ -19,12 +19,13 @@ src_couchdb <- function(host = "127.0.0.1", port = 5984, path = NULL,
                         transport = "http", user = NULL, pwd = NULL,
                         headers = NULL) {
 
-  x <- sofa::Cushion$new(host = host, port = port)
-  x$path <- path
-  x$transport <- transport
-  x$user <- user
-  x$pwd <- pwd
-  x$headers <- headers
+  x <- sofa::Cushion$new(host = host, 
+                         port = port,
+                         path = path,
+                         transport = transport,
+                         user=user, 
+                         pwd=pwd,
+                         headers = headers)
   info <- sofa::ping(x)
   dbs <- sofa::db_list(x)
 
